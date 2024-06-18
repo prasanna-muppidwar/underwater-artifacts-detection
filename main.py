904a91850a8d2243389abecc7135d75217338190
tusharpamnani <tusharpamnani1@gmail.com>
2024-06-19 00:06:49 +0530

    feat: added cam feature

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import cvzone
 import math
 import time
  
-cap = cv2.VideoCapture('natural-form.jpeg') 
+cap = cv2.VideoCapture(0) 
 cap.set(3, 1280)
 cap.set(4, 720)
  
@@ -38,7 +38,7 @@ while True:
     print(f"FPS: {fps}")
  
     cv2.imshow("Image", img)
-    if cv2.waitKey(0) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
